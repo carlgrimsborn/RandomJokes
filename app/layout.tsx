1bbdc907e5eddad84f2e6642b363ddcda918d9d3
Carl Grimsborn <carlgrimsborn@gmail.com>
2024-12-03 18:07:25 +0100

    shadcn setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import './globals.css';
+import '../globals.css';
 import Providers from './providers';
 
 export const metadata = {
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import Counter from './components/counter';
+import { Button } from '@/components/ui/button';
 
 export default function Page() {
 	return (
@@ -7,6 +8,7 @@ export default function Page() {
 			<h1>App Router</h1>
 			<Link href='/home'>Link</Link>
 			<Counter />
+			<Button variant='outline'>Button</Button>
 		</div>
 	);
 }
